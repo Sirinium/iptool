f5374be59c17f03334ba89c039b7ba6e0b1c37a5
Nicolas Vignon <106801568+Sirinium@users.noreply.github.com>
2024-07-01 13:46:10 +0200

    Update UpdateModule.ps1

--- modules/UpdateModule.ps1
+++ modules/UpdateModule.ps1
@@ -1,2 +1,81 @@
-# Version: 1.0.0
-iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Sirinium/iptool/main/install-IPtoolV2.ps1')
+# Version: 1.0.1
+function Update-Module {
+    $moduleName = "IPtool"
+    $modulePath = "$env:USERPROFILE\Documents\WindowsPowerShell\Modules\$moduleName"
+    $baseGitHubUrl = "https://raw.githubusercontent.com/Sirinium/iptool/main/modules"
+    $modules = @("GeoLocation.ps1", "DNSProvider.ps1", "SIPALG.ps1", "SpeedTest.ps1", "UpdateModule.ps1", "Utility.ps1")
+
+    # Fonction pour télécharger un fichier
+    function Get-File {
+        param (
+            [string]$url,
+            [string]$outputPath
+        )
+
+        try {
+            Invoke-WebRequest -Uri $url -OutFile $outputPath
+        } catch {
+            Write-Host "Failed to download $url" -ForegroundColor Red
+            throw
+        }
+    }
+
+    # Fonction pour comparer les versions de fichiers
+    function Compare-Version {
+        param (
+            [string]$localFilePath,
+            [string]$remoteUrl
+        )
+
+        try {
+            $localVersion = (Select-String -Path $localFilePath -Pattern "# Version:" | Select-Object -First 1).Line
+            $remoteVersion = (Invoke-WebRequest -Uri $remoteUrl -UseBasicParsing).Content | Select-String -Pattern "# Version:" | Select-Object -First 1
+
+            $localVersion = $localVersion -replace "# Version: ", ""
+            $remoteVersion = $remoteVersion.Line -replace "# Version: ", ""
+
+            if ($localVersion -ne $remoteVersion) {
+                return $true
+            } else {
+                return $false
+            }
+        } catch {
+            return $true
+        }
+    }
+
+    # Créer le dossier du module s'il n'existe pas déjà
+    if (-not (Test-Path -Path $modulePath)) {
+        New-Item -Path $modulePath -ItemType Directory
+        Write-Host "Created module directory at $modulePath" -ForegroundColor Green
+    }
+
+    # Télécharger les fichiers du module depuis GitHub si la version est différente
+    Write-Host "=== Downloading module files ===" -ForegroundColor Cyan
+    foreach ($module in $modules) {
+        $localFilePath = "$modulePath\$module"
+        $remoteUrl = "$baseGitHubUrl/$module"
+
+        if ((Test-Path -Path $localFilePath) -and (-not (Compare-Version -localFilePath $localFilePath -remoteUrl $remoteUrl))) {
+            Write-Host "$module is up to date." -ForegroundColor Green
+        } else {
+            Write-Host "Updating $module..." -ForegroundColor Yellow
+            Get-File -url $remoteUrl -outputPath $localFilePath
+            Write-Host "Updated $module" -ForegroundColor Green
+        }
+    }
+
+    # Importer le module dans la session PowerShell courante
+    Write-Host "=== Importing module ===" -ForegroundColor Cyan
+    Import-Module $moduleName -Force
+
+    # Récupérer les informations du module
+    $module = Get-Module -Name $moduleName -ListAvailable | Select-Object -First 1
+
+    Write-Host "=== Module Information ===" -ForegroundColor Cyan
+    Write-Host "Name: $($module.Name)" -ForegroundColor Green
+    Write-Host "Version: $($module.Version)" -ForegroundColor Green
+    Write-Host "Author: $($module.Author)" -ForegroundColor Green
+
+    Write-Host "Module $moduleName has been installed and/or updated successfully." -ForegroundColor Green
+}
