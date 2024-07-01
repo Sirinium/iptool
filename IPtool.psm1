d4cb5c2b369048e8da14770bd7b9c5b9693b5846
Nicolas Vignon <106801568+Sirinium@users.noreply.github.com>
2024-07-01 12:32:05 +0200

    Update IPtool.psm1

--- IPtool.psm1
+++ IPtool.psm1
@@ -2,6 +2,10 @@ $modulesPath = "$PSScriptRoot/modules"
 
 Get-ChildItem -Path $modulesPath -Filter *.ps1 | ForEach-Object {
     . $_.FullName
+    # Extraire et afficher la version du module
+    $moduleVersion = (Select-String -Path $_.FullName -Pattern "# Version:" | Select-Object -First 1).Line
+    $moduleVersion = $moduleVersion -replace "# Version: ", ""
+    Write-Host "Loaded module: $($_.Name) (version: $moduleVersion)" -ForegroundColor Green
 }
 
 Export-ModuleMember -Function * -Alias *
