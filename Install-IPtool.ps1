6254ef53580d3d1aa35cb94ab96447cd9da92d7e
Nicolas Vignon <106801568+Sirinium@users.noreply.github.com>
2024-07-01 09:24:49 +0200

    Update Install-IPtool.ps1

--- Install-IPtool.ps1
+++ Install-IPtool.ps1
@@ -1,11 +1,9 @@
-# Définir les variables
 $moduleName = "IPtool"
 $modulePath = "$env:USERPROFILE\Documents\WindowsPowerShell\Modules\$moduleName"
-$psm1Url = "https://raw.githubusercontent.com/Sirinium/iptool/main/IPtool.psm1"
-$psd1Url = "https://raw.githubusercontent.com/Sirinium/iptool/main/IPtool.psd1"
+$modulesPath = "$modulePath\modules"
+$baseUrl = "https://raw.githubusercontent.com/Sirinium/iptool/main/modules"
 
-# Fonction pour télécharger un fichier
-function Get-File {
+function Receive-File {
     param (
         [string]$url,
         [string]$outputPath
@@ -13,34 +11,52 @@ function Get-File {
 
     try {
         Invoke-WebRequest -Uri $url -OutFile $outputPath
+        Write-Host "Downloaded $url" -ForegroundColor Green
     } catch {
         Write-Host "Failed to download $url" -ForegroundColor Red
         throw
     }
 }
 
-# Créer le dossier du module s'il n'existe pas déjà
 if (-not (Test-Path -Path $modulePath)) {
     New-Item -Path $modulePath -ItemType Directory
     Write-Host "Created module directory at $modulePath" -ForegroundColor Green
 }
 
-# Télécharger les fichiers du module depuis GitHub
-Write-Host "Downloading module files..." -ForegroundColor Cyan
-Get-File -url $psm1Url -outputPath "$modulePath\$moduleName.psm1"
-Get-File -url $psd1Url -outputPath "$modulePath\$moduleName.psd1"
+if (-not (Test-Path -Path $modulesPath)) {
+    New-Item -Path $modulesPath -ItemType Directory
+    Write-Host "Created modules directory at $modulesPath" -ForegroundColor Green
+}
 
-# Importer le module dans la session PowerShell courante
-Write-Host "Importing module..." -ForegroundColor Cyan
-Import-Module $moduleName -Force
+Write-Host "=== Downloading module files ===" -ForegroundColor Cyan
+$psm1Url = "https://raw.githubusercontent.com/Sirinium/iptool/main/IPtool.psm1"
+Receive-File -url $psm1Url -outputPath "$modulePath\$moduleName.psm1"
 
-# Récupérer les informations du module
-$module = Get-Module -Name $moduleName -ListAvailable | Select-Object -First 1
+$moduleListUrl = "https://api.github.com/repos/Sirinium/iptool/contents/modules"
+$moduleList = Invoke-RestMethod -Uri $moduleListUrl | Where-Object { $_.name -like '*.ps1' }
 
-Write-Host "=== Module Information ===" -ForegroundColor Cyan
-Write-Host "Name: $($module.Name)" -ForegroundColor Green
-Write-Host "Version: $($module.Version)" -ForegroundColor Green
-Write-Host "Author: $($module.Author)" -ForegroundColor Green
+foreach ($module in $moduleList) {
+    $moduleUrl = $module.download_url
+    Receive-File -url $moduleUrl -outputPath "$modulesPath\$($module.name)"
+}
+
+Write-Host "=== Importing module ===" -ForegroundColor Cyan
+try {
+    Import-Module $moduleName -Force
+    Write-Host "Module $moduleName imported successfully." -ForegroundColor Green
+} catch {
+    Write-Host "Error importing module ${moduleName}: $($_.Exception.Message)" -ForegroundColor Red
+}
+
+Write-Host "=== Retrieving module information ===" -ForegroundColor Cyan
+try {
+    $module = Get-Module -Name $moduleName -ListAvailable | Select-Object -First 1
+    Write-Host "=== Module Information ===" -ForegroundColor Cyan
+    Write-Host "Name: $($module.Name)" -ForegroundColor Green
+    Write-Host "Version: $($module.Version)" -ForegroundColor Green
+    Write-Host "Author: $($module.Author)" -ForegroundColor Green
+} catch {
+    Write-Host "Error retrieving module information: $($_.Exception.Message)" -ForegroundColor Red
+}
 
 Write-Host "Module $moduleName has been installed and/or updated successfully." -ForegroundColor Green
-pause
