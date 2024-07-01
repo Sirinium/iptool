8d4ca6077410ead9e7e5b08451e091f8b75278e2
Nicolas Vignon <106801568+Sirinium@users.noreply.github.com>
2024-07-01 11:02:19 +0200

    Update IPtool.psm1

--- IPtool.psm1
+++ IPtool.psm1
@@ -1,12 +1,6 @@
-$modulesPath = "$PSScriptRoot/modules"
-
+$modulesPath = Join-Path $PSScriptRoot 'modules'
 Get-ChildItem -Path $modulesPath -Filter *.ps1 | ForEach-Object {
-    try {
-        . $_.FullName
-        Write-Host "Loaded module: $($_.Name)" -ForegroundColor Green
-    } catch {
-        Write-Host "Failed to load module: $($_.Name)" -ForegroundColor Red
-    }
+    . $_.FullName
 }
 
-Export-ModuleMember -Function * -Alias *
+Export-ModuleMember -Function *
