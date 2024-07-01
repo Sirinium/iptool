a4c446c023da4d00b666f0e817ea60babdad6433
Nicolas Vignon <106801568+Sirinium@users.noreply.github.com>
2024-07-01 11:50:37 +0200

    Update GeoLocation.ps1

--- modules/GeoLocation.ps1
+++ modules/GeoLocation.ps1
@@ -90,3 +90,4 @@ function Get-MyIP {
         Write-Host "HTTP Request Error: $($_.Exception.Message)" -ForegroundColor Red
     }
 }
+Write-Host "Loaded module: GeoLocation.ps1" -ForegroundColor Green
